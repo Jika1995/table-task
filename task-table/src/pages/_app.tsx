a469c9e6853504a0eb88ff09a8fd17f1f378ea27
Jika1995 <jikulina2016@gmail.com>
2023-08-08 00:32:51 +0600

    package

--- task-table/src/pages/_app.tsx
+++ task-table/src/pages/_app.tsx
@@ -3,21 +3,25 @@ import type { AppProps } from 'next/app';
 import { queryClient } from '@/utils/queryClient';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { MantineProvider } from '@mantine/core';
+import { Head } from 'next/document';
 
 export default function App({ Component, pageProps }: AppProps) {
 
     return (
-        <QueryClientProvider client={queryClient}>
-            <MantineProvider
-                withGlobalStyles
-                withNormalizeCSS
-                theme={{
-                    /** Put your mantine theme override here */
-                    colorScheme: 'light',
-                }}
-            >
-                <Component {...pageProps} />
-            </MantineProvider>
-        </QueryClientProvider>
+        <>
+            <QueryClientProvider client={queryClient}>
+                <MantineProvider
+                    withGlobalStyles
+                    withNormalizeCSS
+                    theme={{
+                        /** Put your mantine theme override here */
+                        colorScheme: 'light',
+                    }}
+                >
+                    <Component {...pageProps} />
+                </MantineProvider>
+            </QueryClientProvider>
+        </>
+
     )
 }
